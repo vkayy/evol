4f917ac8e1eec888614a346ae2d8de3c1fa1f917
vkayy <vkrice2475@gmail.com>
2023-12-29 18:33:28 +0000

    refactor: remove debugging logs

--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -7,42 +7,36 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
 const ProtectedPage = async () => {
-  const { data } = await currentSession();
+	const { data } = await currentSession();
 
-  if (!data.session) {
-    return redirect("/auth");
-  }
+	if (!data.session) {
+		return redirect("/auth");
+	}
 
-  const id = data.session.user.id;
+	const id = data.session.user.id;
 
-  console.log(id)
+	const profile = await db.profile.findUnique({
+		where: {
+			id,
+		},
+	});
 
-  const profile = await db.profile.findUnique({
-    where: {
-      id
-    }
-  })
-  
-  console.log(profile)
-
-  return ( 
-    <div className="flex flex-col justify-center items-center h-full">
-      <h1 className="pt-2 text-4xl font-bold text-rose-900">evol.</h1>
-      <Link
+	return (
+		<div className="flex flex-col justify-center items-center h-full">
+			<h1 className="pt-2 text-4xl font-bold text-rose-900">evol.</h1>
+			<Link
 				href="/"
 				className="text-rose-900 text-center mt-2 relative after:bg-rose-900 after:absolute
         after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all"
 			>
 				home
 			</Link>
-      <div className="h-full w-full flex flex-col justify-center items-center">
-        <h2 className="text-lg p-6">
-          hello, {profile?.name || "friend"}
-        </h2>
-        <AuthLogoutButton />
-      </div>
-    </div>
-   );
-}
- 
+			<div className="h-full w-full flex flex-col justify-center items-center">
+				<h2 className="text-lg p-6">hello, {profile?.name || "friend"}</h2>
+				<AuthLogoutButton />
+			</div>
+		</div>
+	);
+};
+
 export default ProtectedPage;
